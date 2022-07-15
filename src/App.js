450600a2849f5209d29e7a502cf991ff862fef69
chami98 <chamikara98@gmail.com>
2022-07-15 10:28:01 +0530

    Added Files

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Box, TextField } from "@mui/material";
+import { Container } from "@mui/system";
+import VehicleHistoryRecord from "./Components/VehicleHistoryRecord";
+
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import SecondHeader from "./Components/SecondHeader";
+
+function App() {
+  const [value, setValue] = React.useState("chamikara");
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <div className="App">
+      <Container maxWidth="lg">
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={2}
+        >
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+          >
+            <Typography variant="h4" component="h4">
+              Annual Vehicle Inspection Report
+            </Typography>
+          </Box>
+
+          <VehicleHistoryRecord />
+          <SecondHeader/>
+        </Stack>
+      </Container>
+      ;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SecondHeader.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Box } from "@mui/system";
+import { Container, Stack, TextField } from "@mui/material";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+
+
+const SecondHeader = () => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [value, setValue] = React.useState(new Date("2022-08-18T21:11:54"));
+
+  const handleDate = (newValue) => {
+    setValue(newValue);
+  };
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    >
+      •
+    </Box>
+  );
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 17 }} color="text.secondary" gutterBottom>
+            Vehicle History Record
+          </Typography>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Report Number"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Fleet Unit Number"
+              variant="outlined"
+            />
+
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DesktopDatePicker
+                label="Date "
+                inputFormat="MM/dd/yyyy"
+                value={value}
+                onChange={handleDate}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default SecondHeader;
